d567883fc6399f18a00e04f5b64b952b51148a5e
Arturo G. Bruno <arturogbruno@gmail.com>
2020-05-06 18:36:11 +0200

    add form and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Form from "./components/Form";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Form />
+    </>
   );
 }
 
 export default App;
- 
