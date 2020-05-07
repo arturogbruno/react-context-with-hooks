a027e26231819ce6631f023645d8173d8a55321b
Arturo G. Bruno <arturogbruno@gmail.com>
2020-05-07 19:00:11 +0200

    refactor PageContent to functional component

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class PageContent extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isDarkMode } = this.context;
-        const styles = {
-            backgroundColor: isDarkMode ? "#000" : "#fff",
-            width: "100vw",
-            height: "100vh"
-        };
+function PageContent(props) {
+    const { isDarkMode } = useContext(ThemeContext);
 
-        return(
-            <div style={styles}>{this.props.children}</div>
-        );
-    }
+    const styles = {
+        backgroundColor: isDarkMode ? "#000" : "#fff",
+        width: "100vw",
+        height: "100vh"
+    };
+
+    return <div style={styles}>{props.children}</div>;
 }
 
 export default PageContent;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { createContext } from 'react';
 import useToggleState from '../hooks/useToggleState';
 
 const ThemeContext = createContext();
